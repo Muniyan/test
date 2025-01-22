af96870afde13d0a877b17635f28ddb521b10497
Muniyan <50628045+Muniyan@users.noreply.github.com>
2025-01-22 22:03:53 +0530

    Chat UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Muniyan';
+  public roomId: string = '';
+  public messageText: string = '';
+  public messageArray: {user: string, message: string}[] = [];
+  private storageArray = [];
+
+  public isShow: boolean = false;
+  public phone: string = '';
+  public currentUser = {
+    id: 2,
+    name: 'msquare',
+    phone: '9363995200',
+    roomId: {
+      1: 'room-1'
+    }
+  };
+  public selectedUser = {
+    id: 2,
+    name: 'msquare',
+    phone: '9363995200',
+    roomId: {
+      1: 'room-1'
+    }
+  };
+
+  public userList = [
+    {
+      id: 1,
+      name: 'hasina',
+      phone: '9363795012',
+      roomId: {
+        2: 'room-1'
+      }
+    },
+    {
+      id: 2,
+      name: 'msquare',
+      phone: '9363995200',
+      roomId: {
+        1: 'room-1'
+      }
+    }
+  ];
+
+  
+  login(_t28: any) {
+  }
+
+  sendMessage() {
+  }
+    
+  selectUserHandler(arg0: any) {
+  }
 }
